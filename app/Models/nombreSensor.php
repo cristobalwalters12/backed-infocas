d18706107907a29bd973d36a6ecdb103a1f0dd99
cristobalwalters12 <cristobalwalters@gmail.com>
2023-11-14 20:42:15 -0300

    Add new files and update gitignore

---
+++ app/Models/nombreSensor.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class nombreSensor extends Model
+{
+    use HasFactory;
+    protected $table = 'respaldo_nombres_sensores';
+
+    protected $primaryKey = 'id_sensor';
+
+    public $incrementing = true;
+
+    public $timestamps = false;
+
+    protected $fillable = [
+        'nombre_sensor',
+    ];
+
+    public function sensores()
+    {
+        return $this->hasMany(RespaldoSensor::class, 'id_sensor');
+    }
+}
---
+++ app/Models/sensor.php
@@ -0,0 +1,37 @@
+<?php
+
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class RespaldoSensor extends Model
+{
+    protected $table = 'respaldo_sensores';
+
+    protected $primaryKey = 'numero_registro';
+
+    public $incrementing = true;
+
+    public $timestamps = false;
+
+    protected $fillable = [
+        'id_sensor', 
+        'temperatura', 
+        'humedad', 
+        'fecha', 
+        'hora'
+    ];
+
+    protected $casts = [
+        'temperatura' => 'float',
+        'humedad' => 'float',
+        'fecha' => 'date',
+        'hora' => 'time',
+    ];
+
+    public function nombreSensor()
+    {
+        return $this->belongsTo(RespaldoNombreSensor::class, 'id_sensor');
+    }
+}
